c4001d965237bb36ff7d714a7f377b71c395978d
Henk-Jitze Pool <hjpool@xebic.com>
2021-03-30 15:45:17 +0200

    update eaxy

--- eaxy/decode.py
+++ eaxy/decode.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
-input = open('eaxy', 'rb')
-data = input.read()
-input.close()
+key = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '{', '}']
 
-for val in range(0, 256):
-    decoded_array = []
-    for idx in range(0, len(data)):
-        decoded_array.append(data[idx] ^ val)
-
-    output = open('output/' + hex(val), 'wb')
-    output.write(bytes(decoded_array))
-    output.close()
-
+flag = ["-"]*38
+for keys in key:
+    b = bytearray(open('eaxy', 'rb').read())
+    for i in range(len(b)):
+        b[i] ^= int(hex(ord(keys)),16)
+    if "The XOR key you used" in str(b):
+        for i in str(b).split("this is the ")[1:]:
+            flag[int(i[0:2])] = str(keys)
+print("".join(flag))
